994ccbb27c32b1577ae18d30ef1af28f14040518
Francesco Coppola Bove <francesco.coppolabove@assistdigital.com>
2022-04-29 20:58:13 +0200

    added header and hero components

---
+++ pages/collections/[collectionId].js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Link from 'next/Link'
+import { useRouter } from 'next/router'
+
+const Collection = () => {
+  const router = useRouter()
+
+  console.log(router.query)
+  console.log(router.query.collectionId)
+
+  return (
+    <Link href="/">
+      <h2>{router.query.collectionId}</h2>
+    </Link>
+  )
+}
+
+export default Collection
